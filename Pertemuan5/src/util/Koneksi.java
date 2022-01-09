038b955f51923273545aff125b5b7629f145ede5
Ditazuliany <91639691+Ditazuliany@users.noreply.github.com>
2022-01-09 22:00:14 +0700

    Add files via upload

---
+++ Pertemuan5/src/util/Koneksi.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Koneksi {
+   private static Connection databasekoneksi;
+   
+   public static Connection koneksiDB(){
+       if (databasekoneksi==null){
+           try {
+               String DB,user,pass;
+               DB = "jdbc:mysql://localhost:3306/dabar";
+               user = "root";
+               pass = "";
+               
+               databasekoneksi = (Connection) DriverManager.getConnection(DB, user, pass);
+               JOptionPane.showMessageDialog(null, "Koneksi Berhasil");
+           } catch (Exception e) {
+               JOptionPane.showMessageDialog(null, e.getMessage()); 
+           }
+       }  return databasekoneksi;
+   }
+ 
+}
+
+
